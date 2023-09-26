1498f63548fe28ddc4405bf4e82187434e3c344f
Gustavo Kropiwiec <gustavosk17@hotmail.com>
2023-09-26 11:23:32 -0300

    code optimize

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -10,53 +10,48 @@ export class CalculadoraComponent {
     total = 0;
     totalAnual = 0;
     todosChecked = false;
+    impostos = [
+        'inss',
+        'pis',
+        'cofins',
+        'irpj',
+        'csll',
+        'ipi'
+    ];
 
     limparInputs() {
-        if (!(document.getElementById('switch-inss') as HTMLInputElement).checked) (document.getElementById('input-inss') as HTMLInputElement).value = '';
-        if (!(document.getElementById('switch-pis') as HTMLInputElement).checked) (document.getElementById('input-pis') as HTMLInputElement).value = '';
-        if (!(document.getElementById('switch-cofins') as HTMLInputElement).checked) (document.getElementById('input-cofins') as HTMLInputElement).value = '';
-        if (!(document.getElementById('switch-irpj') as HTMLInputElement).checked) (document.getElementById('input-irpj') as HTMLInputElement).value = '';
-        if (!(document.getElementById('switch-csll') as HTMLInputElement).checked) (document.getElementById('input-csll') as HTMLInputElement).value = '';
-        if (!(document.getElementById('switch-ipi') as HTMLInputElement).checked) (document.getElementById('input-ipi') as HTMLInputElement).value = '';
+        for (const imposto of this.impostos) {
+            if (!(document.getElementById(`switch-${imposto}`) as HTMLInputElement).checked) {
+                (document.getElementById(`input-${imposto}`) as HTMLInputElement).value = '';
+            } 
+        }
     }
 
     calcular() {
         this.limparInputs();
-        console.log((document.getElementById('input-inss') as HTMLInputElement).value);
-        console.log((document.getElementById('input-inss') as HTMLInputElement).value.substring(3).replaceAll('.', ''));
         if (!this.todosChecked) {
-            const valorInss = Number((document.getElementById('input-inss') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.'));
-            const valorPis = Number((document.getElementById('input-pis') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.'));
-            const valorCofins = Number((document.getElementById('input-cofins') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.'));
-            const valorIrpj = Number((document.getElementById('input-irpj') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.'));
-            const valorCsll = Number((document.getElementById('input-csll') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.'));
-            const valorIpi = Number((document.getElementById('input-ipi') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.'));
+            const valores: { [key: string]: number } = {};
+            for (const imposto of this.impostos) {
+                valores[imposto] = this.parseInputValue(`input-${imposto}`);
+            }
         
-            this.total = valorInss + valorPis + valorCofins + valorIrpj + valorCsll + valorIpi;
+            this.total = Object.values(valores).reduce((a, b) => a + b, 0);
 
-            this.totalAnual = (valorPis + valorCofins + valorIrpj + valorCsll + valorIpi) * 12 + valorInss * 13;
+            this.totalAnual = (valores['pis'] + valores['cofins'] + valores['irpj'] + valores['csll'] + valores['ipi']) * 12 + valores['inss'] * 13;
         } else {
-            this.total = Number((document.getElementById('input-todos') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.'));
-            this.totalAnual = Number((document.getElementById('input-todos') as HTMLInputElement).value.substring(3).replaceAll('.', '').replace(',', '.')) * 12;
+            const valorTotalTodos = this.parseInputValue('input-todos');
+            this.total = valorTotalTodos;
+            this.totalAnual = valorTotalTodos * 12;
         }
-        console.log(this.total);
     }
 
     changeSwitch(tipo: string) {
         if (tipo == 'todos') {
             this.todosChecked = !this.todosChecked;
-            (document.getElementById('switch-inss') as HTMLInputElement).checked = false;
-            (document.getElementById('switch-pis') as HTMLInputElement).checked = false;
-            (document.getElementById('switch-cofins') as HTMLInputElement).checked = false;
-            (document.getElementById('switch-irpj') as HTMLInputElement).checked = false;
-            (document.getElementById('switch-csll') as HTMLInputElement).checked = false;
-            (document.getElementById('switch-ipi') as HTMLInputElement).checked = false;
-            document.getElementById('ig-inss')?.classList.add('d-none');
-            document.getElementById('ig-pis')?.classList.add('d-none');
-            document.getElementById('ig-cofins')?.classList.add('d-none');
-            document.getElementById('ig-irpj')?.classList.add('d-none');
-            document.getElementById('ig-csll')?.classList.add('d-none');
-            document.getElementById('ig-ipi')?.classList.add('d-none');
+            for (const imposto of this.impostos) {
+                (document.getElementById(`switch-${imposto}`) as HTMLInputElement).checked = false;
+                document.getElementById(`ig-${imposto}`)?.classList.add('d-none');
+            }
             document.getElementById(`ig-${tipo}`)?.classList.toggle('d-none');
             return;
         }
@@ -65,5 +60,10 @@ export class CalculadoraComponent {
 
     mask(event: Event) {
         mascaraMoeda(event);
+    }
+    
+    private parseInputValue(inputId: string): number {
+        const inputElement = document.getElementById(inputId) as HTMLInputElement;
+        return Number(inputElement.value.substring(3).replaceAll('.', '').replace(',', '.'));
     }
 }
